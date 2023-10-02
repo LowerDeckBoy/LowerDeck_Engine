9a399dad662150110011d3eafa7b4693707b92e6
LowerDeckBoy <pa.krawiec@onet.pl>
2023-10-02 18:12:36 +0200

    Added GPU feature checking.

--- LowerDeck Engine/Engine/D3D/D3D12Context.cpp
+++ LowerDeck Engine/Engine/D3D/D3D12Context.cpp
@@ -5,10 +5,9 @@
 namespace D3D
 {
 	std::unique_ptr<D3D12DescriptorHeap> D3D12Context::m_MainHeap = nullptr;
+	std::unique_ptr<D3D12DescriptorHeap> D3D12Context::m_DepthHeap = nullptr;
 
-	D3D12Context::D3D12Context()
-	{
-	}
+	D3D12Context::SupportedFeatures D3D12Context::FeatureSet = {};
 
 	D3D12Context::~D3D12Context()
 	{
@@ -23,11 +22,34 @@ namespace D3D
 			//return false;
 		}
 
-		m_Viewport.Set(Window::m_Resolution.Width, Window::m_Resolution.Height);
+		m_Viewport = new D3D12Viewport();
+		m_Viewport->Set(Window::m_Resolution.Width, Window::m_Resolution.Height);
 
 		D3D::InitializeDevice();
 		D3D::InitializeCommands();
-		D3D::InitializeSwapChain(m_Viewport);
+		D3D::InitializeSwapChain(*m_Viewport);
+
+		InitializeHeaps();
+		CheckFeatures();
+
+		bInitialized = true;
+		return true;
+	}
+
+	bool D3D12Context::InitializeD3D(uint32_t Width, uint32_t Height)
+	{
+		if (IsInitialized())
+		{
+			// warning log
+			//return false;
+		}
+
+		m_Viewport = new D3D12Viewport();
+		m_Viewport->Set(Width, Height);
+
+		D3D::InitializeDevice();
+		D3D::InitializeCommands();
+		D3D::InitializeSwapChain(*m_Viewport);
 
 		InitializeHeaps();
 
@@ -37,7 +59,11 @@ namespace D3D
 
 	void D3D12Context::InitializeHeaps()
 	{
-		m_MainHeap = std::make_unique<D3D12DescriptorHeap>(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 4096, L"Main Descriptor Heap");
+		// SRV
+		m_MainHeap = std::make_unique<D3D12DescriptorHeap>(HeapUsage::eSRV_CBV_UAV, 4096, L"Main Descriptor Heap");
+
+		// DSV
+		m_DepthHeap = std::make_unique<D3D12DescriptorHeap>(HeapUsage::eDSV, 64, L"Main Depth Heap");
 	}
 
 	void D3D12Context::WaitForGPU()
@@ -98,21 +124,15 @@ namespace D3D
 		ResetCommandLists();
 		ReleaseRenderTargets();
 
-		// Reset DepthStencil
-		m_Viewport.Set(Window::m_Resolution.Width, Window::m_Resolution.Height);
-		//static_cast<uint32_t>(m_Viewport.Viewport().Width), static_cast<uint32_t>(m_Viewport.Viewport().Width), 
+		m_Viewport->Set(Window::m_Resolution.Width, Window::m_Resolution.Height);
+
 		ThrowIfFailed(g_SwapChain.Get()->ResizeBuffers(
 			FRAME_COUNT, 
-			Window::m_Resolution.Width, Window::m_Resolution.Height,
+			static_cast<uint32_t>(m_Viewport->Viewport().Width), static_cast<uint32_t>(m_Viewport->Viewport().Height),
 			g_RenderTargetFormat, 0));
 
 		FRAME_INDEX = 0;
-		//uint32_t w;
-		//uint32_t h;
-		//g_SwapChain.Get()->GetSourceSize(&w, &h);
-		//m_Viewport.Set(w, h);
 		CreateBackbuffers();
-		// DepthStencil
 
 		ExecuteCommandLists(false);
 	}
@@ -124,6 +144,9 @@ namespace D3D
 
 		// Despite being a std::unique_ptr Heap is released manualy.
 		// Otherwise other structs are released before Heap causing to false-positive LIVE_DESCRIPTORHEAP.
+		m_DepthHeap.reset();
+		m_DepthHeap = nullptr;
+
 		m_MainHeap.reset();
 		m_MainHeap = nullptr;
 
@@ -144,5 +167,201 @@ namespace D3D
 			g_RenderTargets.at(i).Reset();
 			g_FenceValues.at(i) = g_FenceValues.at(FRAME_INDEX);
 		}
+	}
+
+	void D3D12Context::CheckFeatures()
+	{
+		auto checkShaderSupport = [&]() -> void {
+			D3D12_FEATURE_DATA_SHADER_MODEL shaderModel{ D3D_SHADER_MODEL_6_6 };
+
+			if (SUCCEEDED(g_Device.Get()->CheckFeatureSupport(D3D12_FEATURE_SHADER_MODEL, &shaderModel, sizeof(D3D12_FEATURE_SHADER_MODEL))))
+			{
+#if defined(NTDDI_WIN10_VB) && (NTDDI_VERSION >= NTDDI_WIN10_VB)
+				shaderModel.HighestShaderModel = D3D_SHADER_MODEL_6_6;
+				utility::Print("HLSL Shader Model 6.6 is supported.\n");
+				FeatureSet.ShaderModel = D3D_SHADER_MODEL_6_6;
+#elif defined(NTDDI_WIN10_19H1) && (NTDDI_VERSION >= NTDDI_WIN10_19H1)
+				FeatureSet.ShaderModel = D3D_SHADER_MODEL_6_5;
+				utility::Print("Highest HLSL Shader: Model 6.5.\n");
+#elif defined(NTDDI_WIN10_RS5) && (NTDDI_VERSION >= NTDDI_WIN10_RS5)
+				FeatureSet.ShaderModel = D3D_SHADER_MODEL_6_4;
+				utility::Print("Highest HLSL Shader Model: 6.4.\n");
+#elif defined(NTDDI_WIN10_RS4) && (NTDDI_VERSION >= NTDDI_WIN10_RS4)
+				FeatureSet.ShaderModel = D3D_SHADER_MODEL_6_2;
+				utility::Print("Highest HLSL Shader: Model 6.2.\n");
+#elif defined(NTDDI_WIN10_RS3) && (NTDDI_VERSION >= NTDDI_WIN10_RS3)
+				FeatureSet.ShaderModel = D3D_SHADER_MODEL_6_1;
+				utility::Print("Highest HLSL Shader: Model 6.1.\n");
+#else
+				FeatureSet.ShaderModel = D3D_SHADER_MODEL_6_0;
+				utility::Print("Highest HLSL Shader: Model 6.0.\n");
+#endif
+			}
+			};
+		checkShaderSupport();
+
+		D3D12_FEATURE_DATA_D3D12_OPTIONS featuresSupport{};
+			
+		ThrowIfFailed(g_Device.Get()->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS, &featuresSupport, sizeof(D3D12_FEATURE_DATA_D3D12_OPTIONS)), "Failed to enumarate GPU features!\n");
+			
+		// ResourceHeapTier
+		{
+			switch (featuresSupport.ResourceHeapTier)
+			{
+			case D3D12_RESOURCE_HEAP_TIER_2:
+			{
+				utility::Print("D3D12 Resource Heap Tier 2 is supported.\n");
+				FeatureSet.HeapTier = D3D12_RESOURCE_HEAP_TIER_2;
+				break;
+			}
+			case D3D12_RESOURCE_HEAP_TIER_1:
+			{
+				utility::Print("D3D12 Resource Heap Tier 1 is supported.\n");
+				FeatureSet.HeapTier = D3D12_RESOURCE_HEAP_TIER_1;
+				break;
+			}
+			}
+		}
+
+		// ResourceBindingTier
+		{
+			switch (featuresSupport.ResourceBindingTier)
+			{
+			case D3D12_RESOURCE_BINDING_TIER_3:
+			{
+				utility::Print("D3D12 Resource Binding Tier 3 is supported.\n");
+				FeatureSet.BindingTier = D3D12_RESOURCE_BINDING_TIER_3;
+				break;
+			}
+			case D3D12_RESOURCE_BINDING_TIER_2:
+			{
+				utility::Print("D3D12 Resource Binding Tier 2 is supported.\n");
+				FeatureSet.BindingTier = D3D12_RESOURCE_BINDING_TIER_2;
+				break;
+			}
+			case D3D12_RESOURCE_BINDING_TIER_1:
+			{
+				utility::Print("D3D12 Resource Binding Tier 1 is supported.\n");
+				FeatureSet.BindingTier = D3D12_RESOURCE_BINDING_TIER_1;
+				break;
+			}
+			}
+		}
+
+		// TiledResourcesTier
+		{
+			switch (featuresSupport.TiledResourcesTier)
+			{
+			case D3D12_TILED_RESOURCES_TIER_4:
+			{
+				utility::Print("D3D12 Tiled Resource Tier 4 is supported.\n");
+				FeatureSet.TiledResourcesTier = D3D12_TILED_RESOURCES_TIER_4;
+				break;
+			}
+			case D3D12_TILED_RESOURCES_TIER_3:
+			{
+				utility::Print("D3D12 Tiled Resource Tier 3 is supported.\n");
+				FeatureSet.TiledResourcesTier = D3D12_TILED_RESOURCES_TIER_3;
+				break;
+			}
+			case D3D12_TILED_RESOURCES_TIER_2:
+			{
+				utility::Print("D3D12 Tiled Resource Tier 2 is supported.\n");
+				FeatureSet.TiledResourcesTier = D3D12_TILED_RESOURCES_TIER_2;
+				break;
+			}
+			case D3D12_TILED_RESOURCES_TIER_1:
+			{
+				utility::Print("D3D12 Tiled Resource Tier 1 is supported.\n");
+				FeatureSet.TiledResourcesTier = D3D12_TILED_RESOURCES_TIER_1;
+				break;
+			}
+			default:
+			{
+				utility::Print("D3D12 Tiled Resource is NOT supported.\n");
+				FeatureSet.TiledResourcesTier = D3D12_TILED_RESOURCES_TIER_NOT_SUPPORTED;
+				break;
+			}
+			}
+		}
+
+		D3D12_FEATURE_DATA_ARCHITECTURE1 dataArchitecture{};
+		ThrowIfFailed(g_Device.Get()->CheckFeatureSupport(D3D12_FEATURE_ARCHITECTURE1, &dataArchitecture, sizeof(dataArchitecture)));
+
+		// Data Architecture
+		{
+			if (dataArchitecture.TileBasedRenderer == TRUE)
+			{ 
+				utility::Print("D3D12 Tile Based Renderer available.\n");
+				FeatureSet.bTileBasedRenderer = TRUE;
+			}
+			else
+			{
+				utility::Print("D3D12 Tile Based Renderer NOT available.\n");
+				FeatureSet.bTileBasedRenderer = FALSE;
+			}
+
+			if (dataArchitecture.UMA == TRUE)
+			{
+				utility::Print("D3D12 UMA available.\n");
+				FeatureSet.bUMA = TRUE;
+			}
+			else
+			{
+				utility::Print("D3D12 UMA NOT available.\n");
+				FeatureSet.bUMA = FALSE;
+			}
+		}
+
+		D3D12_FEATURE_DATA_D3D12_OPTIONS5 featuresSupport5{};
+		ThrowIfFailed(g_Device.Get()->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS5, &featuresSupport5, sizeof(featuresSupport5)), "Failed to enumerate D3D12_FEATURE_DATA_D3D12_OPTIONS5!\n");
+		// RenderPasses
+		{
+			switch (featuresSupport5.RenderPassesTier)
+			{
+			case D3D12_RENDER_PASS_TIER_2:
+			{
+				utility::Print("D3D12 Render Pass Tier 2 is supported.\n");
+				FeatureSet.RenderPassTier = D3D12_RENDER_PASS_TIER_2;
+				break;
+			}
+			case D3D12_RENDER_PASS_TIER_1:
+			{
+				utility::Print("D3D12 Render Pass Tier 1 is supported.\n");
+				FeatureSet.RenderPassTier = D3D12_RENDER_PASS_TIER_1;
+				break;
+			}
+			case D3D12_RENDER_PASS_TIER_0:
+			{
+				utility::Print("D3D12 Render Pass is NOT supported.\n");
+				FeatureSet.RenderPassTier = D3D12_RENDER_PASS_TIER_0;
+				break;
+			}
+			}
+		}
+
+		D3D12_FEATURE_DATA_D3D12_OPTIONS7 featuresSupport7{};
+		ThrowIfFailed(g_Device.Get()->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS7, &featuresSupport7, sizeof(featuresSupport7)));
+		// Mesh shaders
+		{
+			switch (featuresSupport7.MeshShaderTier)
+			{
+			case D3D12_MESH_SHADER_TIER_1:
+			{
+				utility::Print("D3D12 Mesh Shader Tier 1 is supported.\n");
+				FeatureSet.MeshShaderTier = D3D12_MESH_SHADER_TIER_1;
+				break;
+			}
+			default:
+			{
+				utility::Print("D3D12 Mesh Shader is NOT supported.\n");
+				FeatureSet.MeshShaderTier = D3D12_MESH_SHADER_TIER_NOT_SUPPORTED;
+				break;
+			}
+			}
+		}
+
+		// TODO:
+		// if raytracing D3D12_FEATURE_DATA_D3D12_OPTIONS3::BarycentricsSupported
 	}
 }
--- LowerDeck Engine/Engine/D3D/D3D12Context.hpp
+++ LowerDeck Engine/Engine/D3D/D3D12Context.hpp
@@ -11,30 +11,44 @@ namespace D3D
 	/// Main class (entry point) for DirectX 12 based context.<br/>
 	/// All pointers to DX structures are initialized here,
 	/// as well as they are cleanup here via class destructor.<br/>
-	/// It is meant for simplying and securing both context creation and it's later cleanup.
+	/// Class is meant for simplying and securing both context creation and it's later cleanup.
 	/// </summary>
 	class D3D12Context
 	{
 	public:
-		D3D12Context();
+		D3D12Context() = default;
 		D3D12Context(const D3D12Context&) = delete;
 		D3D12Context(const D3D12Context&&) = delete;
 		D3D12Context operator=(const D3D12Context&) = delete;
 		~D3D12Context();
 
 		bool InitializeD3D();
+		bool InitializeD3D(uint32_t Width, uint32_t Height);
 
 		void InitializeHeaps();
 
+		/// <summary>
+		/// Wait until <c>Fence</c> event is complete.
+		/// </summary>
 		void WaitForGPU();
+		/// <summary>
+		/// Move to next <c>SwapChain Backbuffer</c><br/>.
+		/// If frame is not ready to be presented, wait until it is.
+		/// </summary>
 		void MoveToNextFrame();
+		/// <summary>
+		/// Clear <c>Fence Event values</c> then move back to first backbuffer.
+		/// </summary>
 		void FlushGPU();
 
+		/// <summary>
+		/// Resize <c>Scene Viewport</c> and resize all <c>SwapChain Backbuffers</c>.
+		/// </summary>
 		void OnResize();
 
 		/// <summary>
-		/// Safe release of all member ComPtrs
-		/// and other DirectX based structs.
+		/// Safe release of all member <c>ComPtrs</c> and other DirectX based structs.<br/>
+		/// Note: If in Debug mode, check for live objects.
 		/// </summary>
 		void ReleaseD3D();
 
@@ -44,12 +58,26 @@ namespace D3D
 		/// <returns> <b>True</b> if was already initialized. </returns>
 		bool IsInitialized();
 
-		inline static D3D12DescriptorHeap* GetMainHeap() { return m_MainHeap.get(); }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns> Pointer to SRV/CBV/UAV Descriptor Heap. </returns>
+		[[nodiscard]] inline static D3D12DescriptorHeap* GetMainHeap() { return m_MainHeap.get(); }
 
-		const D3D12Viewport& GetSceneViewport() const { return m_Viewport; }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns> Pointer to general use DSV Descriptor Heap. </returns>
+		[[nodiscard]] inline static D3D12DescriptorHeap* GetDepthHeap() { return m_DepthHeap.get(); }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns> Pointer to Scene Viewport. </returns>
+		D3D12Viewport* GetSceneViewport() const { return m_Viewport; }
 
 	protected:
-		D3D12Viewport m_Viewport;
+		D3D12Viewport* m_Viewport{ nullptr };
 
 		bool bInitialized{ false };
 
@@ -60,9 +88,70 @@ namespace D3D
 		/// </summary>
 		void ReleaseRenderTargets();
 
-		// CBV_SRV_UAV heap
-		// Main source of resource allocations
+		// TODO:
+		// https://github.com/microsoft/DirectXTK12/wiki/Shader-Model-6
+		// https://learn.microsoft.com/en-us/windows/win32/api/d3d12/ns-d3d12-d3d12_feature_data_d3d12_options
+		/// <summary>
+		/// Query GPU capabilities.<br/>
+		/// Validates if current GPU does provide necessary features to run the engine.<br/>
+		/// Features that must be supported:<br/>
+		/// - Shader Model 6.0+. Desired 6.6 support, but 6.3 will suffice for current needs.<br/>
+		/// - Raytracing support.<br/>
+		/// </summary>
+		void CheckFeatures();
+
+		/// <summary>
+		/// CBV_SRV_UAV heap.<br/>Main source of resource allocations.
+		/// </summary>
 		static std::unique_ptr<D3D12DescriptorHeap> m_MainHeap;
+		/// <summary>
+		/// DSV heap.
+		/// </summary>
+		static std::unique_ptr<D3D12DescriptorHeap> m_DepthHeap;
+
+		/// <summary>
+		/// Supported and desired Device features
+		/// </summary>
+		struct SupportedFeatures
+		{
+			/// <summary>
+			/// Highest supported shader model.<br/>
+			/// Desired level: 6.6.
+			/// </summary>
+			D3D_SHADER_MODEL ShaderModel;
+			/// <summary>
+			/// 
+			/// </summary>
+			D3D12_MESH_SHADER_TIER MeshShaderTier;
+			/// <summary>
+			/// 
+			/// </summary>
+			D3D12_RESOURCE_HEAP_TIER HeapTier;
+			/// <summary>
+			/// 
+			/// </summary>
+			D3D12_RESOURCE_BINDING_TIER BindingTier;
+			/// <summary>
+			/// 
+			/// </summary>
+			D3D12_TILED_RESOURCES_TIER TiledResourcesTier;
+			/// <summary>
+			/// 
+			/// </summary>
+			D3D12_RENDER_PASS_TIER RenderPassTier;
+			/// <summary>
+			/// 
+			/// </summary>
+			BOOL bTileBasedRenderer;
+			/// <summary>
+			/// 
+			/// </summary>
+			BOOL bUMA;
+
+			DXGI_FORMAT DisplayFormat;
+		};
+		
+		static SupportedFeatures FeatureSet;
 
 	};
 }
