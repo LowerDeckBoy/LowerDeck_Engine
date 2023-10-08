d343ba95cf4efdde77854c9a68f67bb060463633
LowerDeckBoy <pa.krawiec@onet.pl>
2023-10-08 18:51:16 +0200

    New helper functions

--- LowerDeck Engine/Engine/Graphics/TextureUtility.cpp
+++ LowerDeck Engine/Engine/Graphics/TextureUtility.cpp
@@ -4,6 +4,7 @@
 #include <directxtk12/DDSTextureLoader.h>
 #include <directxtk12/ResourceUploadBatch.h>
 #include "../D3D/D3D12Context.hpp"
+#include "../Utility/FileSystem.hpp"
 #include "../Utility/Utility.hpp"
 
 
@@ -22,16 +23,15 @@ void TextureUtility::CreateFromWIC(const std::string_view& Filepath, ID3D12Resou
 		ppResource));
 
 	const auto desc{ (*ppResource)->GetDesc()};
-
 	uint16_t mipLevel = desc.MipLevels;
-
+	
 	// Generate Mipmaps ONLY for those textures which have SUITABLE format.
 	// Else hardcode mip levels to 1, so SRVs are generated correctly.
 	if (upload.IsSupportedForGenerateMips(desc.Format))
 		upload.GenerateMips((*ppResource));
 	else
 		mipLevel = 1;
-
+	
 	auto finish{ upload.End(D3D::g_CommandQueue.Get()) };
 	finish.wait();
 	CreateSRV(ppResource, Descriptor, mipLevel, desc.Format);
@@ -42,25 +42,183 @@ void TextureUtility::CreateFromDDS(const std::string_view& Filepath, ID3D12Resou
 {
 }
 
-void TextureUtility::CreateFromHDR(const std::string_view& Filepath, ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor)
+void TextureUtility::CreateFromHDR(const std::string_view& Filepath, ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, bool bGenerateMipChain)
 {
+	if (utility::GetFileExtension(Filepath) != ".hdr")
+	{
+		// message here
+	}
+
+	std::wstring path{ utility::ToWideString(Filepath) };
+	DirectX::ScratchImage scratchImage{};
+	DirectX::LoadFromHDRFile(path.c_str(), nullptr, scratchImage);
+
+	// TEST
+	//if (bGenerateMipChain)
+	//{
+	//	scratchImage = GenerateMipChain(scratchImage, 6, false);
+	//}
+	DirectX::TexMetadata metadata{ scratchImage.GetMetadata() };
+
+	D3D12_RESOURCE_DESC desc{};
+	desc.Format		= metadata.format;
+	desc.Width		= static_cast<uint32_t>(metadata.width);
+	desc.Height		= static_cast<uint32_t>(metadata.height);
+	desc.MipLevels	= scratchImage.GetMetadata().mipLevels;
+	//desc.MipLevels	= 1;
+	desc.DepthOrArraySize = metadata.IsCubemap() ? static_cast<uint16_t>(metadata.arraySize) : static_cast<uint16_t>(metadata.depth);
+	desc.SampleDesc = { 1, 0 };
+	desc.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+
+	if (metadata.dimension == DirectX::TEX_DIMENSION_TEXTURE1D)
+		desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE1D;
+	else if (metadata.dimension == DirectX::TEX_DIMENSION_TEXTURE2D)
+		desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	else if (metadata.dimension == DirectX::TEX_DIMENSION_TEXTURE3D)
+		desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE3D;
+
+	ThrowIfFailed(D3D::g_Device.Get()->CreateCommittedResource(
+		&D3D::Utility::HeapDefault,
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		D3D12_RESOURCE_STATE_COPY_DEST,
+		nullptr,
+		IID_PPV_ARGS(ppResource)));
+
+	const uint64_t bufferSize{ ::GetRequiredIntermediateSize((*ppResource), 0, 1) };
+
+	ID3D12Resource* uploadHeap{ nullptr };
+	const auto bufferDesc{ CD3DX12_RESOURCE_DESC::Buffer(bufferSize) };
+	ThrowIfFailed(D3D::g_Device.Get()->CreateCommittedResource(
+		&D3D::Utility::HeapUpload,
+		D3D12_HEAP_FLAG_NONE,
+		&bufferDesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&uploadHeap)));
+
+	D3D12_SUBRESOURCE_DATA subresource{};
+	subresource.pData		= scratchImage.GetImages()->pixels;
+	subresource.RowPitch	= static_cast<int64_t>(scratchImage.GetImages()->rowPitch);
+	subresource.SlicePitch	= static_cast<int64_t>(scratchImage.GetImages()->slicePitch);
+
+	::UpdateSubresources(D3D::g_CommandList.Get(), (*ppResource), uploadHeap, 0, 0, 1, &subresource);
+
+	D3D::ExecuteCommandLists(true);
+
+	D3D::TransitResource((*ppResource), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_GENERIC_READ);
+	// Delete AFTER executing Command Lists, otherwise upload resource will remain live.
+	SAFE_DELETE(uploadHeap);
+
 }
 
-void TextureUtility::CreateSRV(ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, uint16_t Mips, DXGI_FORMAT Format)
+ID3D12Resource* TextureUtility::CreateResource(TextureData Data, TextureDesc Desc)
+{
+	D3D12_RESOURCE_DESC resourceDesc{};
+	resourceDesc.Flags		= Desc.Flag;
+	resourceDesc.Format		= Data.Format;
+	resourceDesc.Width		= Data.Width;
+	resourceDesc.Height		= Data.Height;
+	resourceDesc.Dimension	= Data.Dimension;
+	resourceDesc.MipLevels	= 1;
+	resourceDesc.DepthOrArraySize = Data.Depth;
+	resourceDesc.SampleDesc = { 1, 0 };
+
+	ID3D12Resource* resource{ nullptr };
+	ThrowIfFailed(D3D::g_Device.Get()->CreateCommittedResource(
+		&Desc.HeapProperties,
+		Desc.HeapFlag,
+		&resourceDesc,
+		Desc.State,
+		nullptr,
+		IID_PPV_ARGS(&resource)
+	));
+
+	return resource;
+}
+
+void TextureUtility::CreateResource(ID3D12Resource** ppTarget, TextureData Data, TextureDesc Desc)
+{
+	D3D12_RESOURCE_DESC resourceDesc{};
+	resourceDesc.Flags		= Desc.Flag;
+	resourceDesc.Format		= Data.Format;
+	resourceDesc.Width		= Data.Width;
+	resourceDesc.Height		= Data.Height;
+	resourceDesc.Dimension	= Data.Dimension;
+	resourceDesc.MipLevels	= Data.MipLevels;
+	resourceDesc.DepthOrArraySize = Data.Depth;
+	resourceDesc.SampleDesc = { 1, 0 };
+
+	ThrowIfFailed(D3D::g_Device.Get()->CreateCommittedResource(
+		&Desc.HeapProperties,
+		Desc.HeapFlag,
+		&resourceDesc,
+		Desc.State,
+		nullptr,
+		IID_PPV_ARGS(ppTarget)
+	));
+}
+
+void TextureUtility::CreateFromWIC(const std::string_view& Filepath, ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, bool bMips)
+{
+	auto path{ utility::ToWideString(Filepath) };
+
+	DirectX::ResourceUploadBatch upload(D3D::g_Device.Get());
+	upload.Begin();
+	//DirectX::LoadWICTextureFromMemoryEx()
+	//ThrowIfFailed(DirectX::LoadFro)
+
+}
+
+DirectX::ScratchImage TextureUtility::GenerateMipChain(DirectX::ScratchImage& BaseImage, uint16_t MipLevels, bool bIsWIC)
+{
+	DirectX::ScratchImage mipChain;
+	DirectX::TexMetadata metadata{};
+
+	auto flags{ (!bIsWIC) ? DirectX::TEX_FILTER_FORCE_NON_WIC : DirectX::TEX_FILTER_DEFAULT };
+	ThrowIfFailed(DirectX::GenerateMipMaps(BaseImage.GetImages(), BaseImage.GetImageCount(), BaseImage.GetMetadata(), flags, (size_t)MipLevels, mipChain));
+	
+	return mipChain;
+}
+
+void TextureUtility::CreateSRV(ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, uint16_t Mips, DXGI_FORMAT Format, D3D12_SRV_DIMENSION ViewDimension)
 {
 	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
 	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 	srvDesc.Format = Format;
-	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-	srvDesc.Texture2D.MipLevels = Mips;
-	srvDesc.Texture2D.MostDetailedMip = 0;
+	if (ViewDimension == D3D12_SRV_DIMENSION_TEXTURE2D)
+	{
+		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+		srvDesc.Texture2D.MipLevels = Mips;
+		srvDesc.Texture2D.MostDetailedMip = 0;
+	}
+	else if (ViewDimension == D3D12_SRV_DIMENSION_TEXTURECUBE)
+	{
+		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURECUBE;
+		srvDesc.TextureCube.MipLevels = Mips;
+		srvDesc.TextureCube.MostDetailedMip = 0;
+	}
 
 	D3D::D3D12Context::GetMainHeap()->Allocate(Descriptor);
 	D3D::g_Device.Get()->CreateShaderResourceView((*ppResource), &srvDesc, Descriptor.GetCPU());
 }
 
-void TextureUtility::CreateUAV(ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, DXGI_FORMAT Format)
+void TextureUtility::CreateUAV(ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, uint16_t Depth, DXGI_FORMAT Format)
 {
 	D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc{};
-	
+	uavDesc.Format = Format;
+	if (Depth == 1)
+	{
+		uavDesc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
+		uavDesc.Texture2D.MipSlice = 0;
+	}
+	else if (Depth == 6)
+	{
+		uavDesc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2DARRAY;
+		uavDesc.Texture2DArray.MipSlice = 0;
+		uavDesc.Texture2DArray.FirstArraySlice = 0;
+		uavDesc.Texture2DArray.ArraySize = 1;
+	}
+
+	D3D::g_Device.Get()->CreateUnorderedAccessView((*ppResource), nullptr, &uavDesc, Descriptor.GetCPU());
 }
--- LowerDeck Engine/Engine/Graphics/TextureUtility.hpp
+++ LowerDeck Engine/Engine/Graphics/TextureUtility.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string_view>
 #include <d3d12.h>
+#include <d3dx12.h>
 #include <wrl/client.h>
 
 //struct ID3D12Resource;
@@ -9,7 +10,11 @@ namespace D3D
 	class D3D12Descriptor;
 }
 
-/*
+namespace DirectX
+{
+	class ScratchImage;
+}
+
 struct TextureData
 {
 	uint64_t Width;
@@ -24,10 +29,10 @@ struct TextureDesc
 {
 	D3D12_RESOURCE_FLAGS	Flag{ D3D12_RESOURCE_FLAG_NONE };
 	D3D12_RESOURCE_STATES	State{ D3D12_RESOURCE_STATE_COMMON };
-	//CD3DX12_HEAP_PROPERTIES HeapProperties{ CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT) };
+	CD3DX12_HEAP_PROPERTIES HeapProperties{ CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT) };
 	D3D12_HEAP_FLAGS		HeapFlag{ D3D12_HEAP_FLAG_NONE };
 };
-*/
+
 class TextureUtility
 {
 public:
@@ -42,9 +47,13 @@ public:
 	// Note: testing required
 	static void CreateFromDDS(const std::string_view& Filepath, ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor);
 	// Note: testing required
-	static void CreateFromHDR(const std::string_view& Filepath, ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor);
+	static void CreateFromHDR(const std::string_view& Filepath, ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, bool bGenerateMipChain = false);
 
-	//static ID3D12Resource* CreateResource();
+	static ID3D12Resource* CreateResource(TextureData Data, TextureDesc Desc);
+	static void CreateResource(ID3D12Resource** ppTarget, TextureData Data, TextureDesc Desc);
+
+	static void CreateFromWIC(const std::string_view& Filepath, ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, bool bMips);
+	static DirectX::ScratchImage GenerateMipChain(DirectX::ScratchImage& BaseImage, uint16_t MipLevels, bool bIsWIC = true);
 
 	/// <summary>
 	/// Create <c>ShaderResourceView</c> for given <c>Texture Resource</c> and it's <c>Descriptor</c>.
@@ -53,7 +62,7 @@ public:
 	/// <param name="Descriptor"> Target Descriptor. </param>
 	/// <param name="Mips"> Level of MipMap chain.</param>
 	/// <param name="Format"> Texture format. Note: not every format supports mipmap generation. </param>
-	static void CreateSRV(ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, uint16_t Mips, DXGI_FORMAT Format = DXGI_FORMAT_R8G8B8A8_UNORM);
+	static void CreateSRV(ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, uint16_t Mips, DXGI_FORMAT Format = DXGI_FORMAT_R8G8B8A8_UNORM, D3D12_SRV_DIMENSION ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D);
 
 	/// <summary>
 	/// Create <c>UnorderedAccessView</c> for given <c>Texture Resource</c> and it's <c>Descriptor</c>.
@@ -61,6 +70,6 @@ public:
 	/// <param name="ppResource"> Target Resource. Usually <c>ComPtr</c>. </param>
 	/// <param name="Descriptor"> Target Descriptor. </param>
 	/// <param name="Format"> Texture format. </param>
-	static void CreateUAV(ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, DXGI_FORMAT Format = DXGI_FORMAT_R8G8B8A8_UNORM);
+	static void CreateUAV(ID3D12Resource** ppResource, D3D::D3D12Descriptor& Descriptor, uint16_t Depth, DXGI_FORMAT Format = DXGI_FORMAT_R8G8B8A8_UNORM);
 
 };
