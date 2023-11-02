18144ee76de19dffedddca306c639c4bc9588931
LowerDeckBoy <pa.krawiec@onet.pl>
2023-11-02 18:58:28 +0100

    Cleanup

--- LowerDeck Engine/Engine/Graphics/MipMapGenerator.cpp
+++ LowerDeck Engine/Engine/Graphics/MipMapGenerator.cpp
@@ -1,6 +1,8 @@
 #include "MipMapGenerator.hpp"
 #include "../D3D/D3D12Device.hpp"
 #include "../D3D/D3D12Descriptor.hpp"
+#include "../D3D/D3D12RootSignature.hpp"
+#include "../D3D/D3D12PipelineState.hpp"
 #include "../D3D/D3D12Context.hpp"
 #include "TextureUtility.hpp"
 #include "../Utility/Math.hpp"
@@ -8,82 +10,62 @@
 #include "Shader5.hpp"
 #include <algorithm>
 
-
 #include <DirectXMath.h>
 using namespace DirectX;
 
 ID3D12RootSignature* MipMapGenerator::m_RootSignature = nullptr;
 ID3D12PipelineState* MipMapGenerator::m_ComputePipeline = nullptr;
-ID3D12Resource*		 MipMapGenerator::m_Textures[4] = {};
-
-D3D::D3D12Descriptor MipMapGenerator::srvDescriptors = {};
-D3D::D3D12Descriptor MipMapGenerator::uavDescriptors = {};
 
 void MipMapGenerator::CreateComputeState(std::weak_ptr<gfx::ShaderManager> wpShaderManager)
 {
-	CD3DX12_DESCRIPTOR_RANGE1 ranges[2] = {};
-	ranges[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 4096, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE | D3D12_DESCRIPTOR_RANGE_FLAG_DATA_VOLATILE, 0);
-	ranges[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 4096, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DESCRIPTORS_VOLATILE | D3D12_DESCRIPTOR_RANGE_FLAG_DATA_VOLATILE, 0);
-
-	CD3DX12_ROOT_PARAMETER1 rootParameters[1] = {}; // 3
+	CD3DX12_ROOT_PARAMETER1 rootParameters[1]{};
 	rootParameters[0].InitAsConstants(8, 0);
-	//rootParameters[1].InitAsDescriptorTable(1, &ranges[0]);
-	//rootParameters[2].InitAsDescriptorTable(1, &ranges[1]);
 
 	D3D12_ROOT_SIGNATURE_FLAGS rootSignatureFlags = D3D12_ROOT_SIGNATURE_FLAG_CBV_SRV_UAV_HEAP_DIRECTLY_INDEXED;
 
-	CD3DX12_STATIC_SAMPLER_DESC staticSamplers[1] = {};
-	staticSamplers[0].Filter = D3D12_FILTER_ANISOTROPIC;
-	staticSamplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
-	staticSamplers[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
-	staticSamplers[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
-	staticSamplers[0].MaxAnisotropy = D3D12_REQ_MAXANISOTROPY;
-	staticSamplers[0].ComparisonFunc = D3D12_COMPARISON_FUNC_ALWAYS;
-	staticSamplers[0].BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
-	staticSamplers[0].MinLOD = 0.0f;
-	staticSamplers[0].MaxLOD = D3D12_FLOAT32_MAX;
-	staticSamplers[0].MipLODBias = 0;
-	staticSamplers[0].ShaderRegister = 0;
-	staticSamplers[0].RegisterSpace = 0;
-	staticSamplers[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+	CD3DX12_STATIC_SAMPLER_DESC staticSampler{};
+	staticSampler.Filter = D3D12_FILTER_ANISOTROPIC;
+	staticSampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
+	staticSampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
+	staticSampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
+	staticSampler.MaxAnisotropy = D3D12_REQ_MAXANISOTROPY;
+	staticSampler.ComparisonFunc = D3D12_COMPARISON_FUNC_ALWAYS;
+	staticSampler.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
+	staticSampler.MinLOD = 0.0f;
+	staticSampler.MaxLOD = D3D12_FLOAT32_MAX;
+	staticSampler.MipLODBias = 0;
+	staticSampler.ShaderRegister = 0;
+	staticSampler.RegisterSpace = 0;
+	staticSampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 
 	CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC versionedRootSignatureDesc = {};
-	versionedRootSignatureDesc.Init_1_1(_countof(rootParameters), &rootParameters[0], _countof(staticSamplers), &staticSamplers[0], rootSignatureFlags);
+	versionedRootSignatureDesc.Init_1_1(_countof(rootParameters), &rootParameters[0], 1, &staticSampler, rootSignatureFlags);
 
 	ComPtr<ID3DBlob> serializedRootSig;
 	ComPtr<ID3DBlob> errorBlob;
 
-	HRESULT hr = D3D12SerializeVersionedRootSignature(&versionedRootSignatureDesc, &serializedRootSig, &errorBlob);
-	if (!SUCCEEDED(hr) || errorBlob)
-	{
-		//LOG_ERR(static_cast<const char*>(errorBlob->GetBufferPointer()));
-		errorBlob->Release();
-	}
+	ThrowIfFailed(D3D12SerializeVersionedRootSignature(&versionedRootSignatureDesc, &serializedRootSig, &errorBlob));
 
-	ThrowIfFailed(D3D::g_Device->CreateRootSignature(0, serializedRootSig->GetBufferPointer(),
-													 serializedRootSig->GetBufferSize(), IID_PPV_ARGS(&m_RootSignature)));
-	//s_Data.MipMapGenRootSig->SetName(StringHelper::StringToWString("Mip map root signature").c_str());
+	ThrowIfFailed(D3D::g_Device->CreateRootSignature(0, 
+		serializedRootSig->GetBufferPointer(),
+		serializedRootSig->GetBufferSize(),
+		IID_PPV_ARGS(&m_RootSignature)));
 
-	gfx::Shader computeShader = wpShaderManager.lock()->CompileDXIL("Shaders/MipMaps_CS.hlsl", ShaderType::eCompute);
-	//s_Data.MipMapGenShader = std::make_unique<Shader>(L"Resources/Shaders/MipMapGen_CS.hlsl", "main", "cs_6_0");
+	m_ComputeShader = new gfx::Shader(wpShaderManager.lock()->CompileDXIL("Shaders/MipMaps_CS.hlsl", ShaderType::eCompute));
 
 	D3D12_COMPUTE_PIPELINE_STATE_DESC psoDesc = {};
-	//psoDesc.pRootSignature = s_Data.MipMapGenRootSig.Get();
-	//psoDesc.CS = s_Data.MipMapGenShader->GetShaderByteCode();
 	psoDesc.pRootSignature = m_RootSignature;
-	psoDesc.CS = computeShader.Bytecode();
+	psoDesc.CS = m_ComputeShader->Bytecode();
 	psoDesc.NodeMask = 0;
 	psoDesc.Flags = D3D12_PIPELINE_STATE_FLAG_NONE;
 
-	//DX_CALL(s_Data.D3D12Device2->CreateComputePipelineState(&psoDesc, IID_PPV_ARGS(&s_Data.MipMapGenPSO)));
 	ThrowIfFailed(D3D::g_Device->CreateComputePipelineState(&psoDesc, IID_PPV_ARGS(&m_ComputePipeline)));
 
 }
 
 void MipMapGenerator::Generate2D(Texture& Texture)
 {
-	auto& srcResource = Texture.m_Resource;
-	if (!srcResource)
+	if (!Texture.m_Resource)
 		return;
 
 	auto srcResourceDesc = Texture.m_Resource->GetDesc();
@@ -99,11 +81,9 @@ void MipMapGenerator::Generate2D(Texture& Texture)
 		return;
 	}
 
-	ID3D12Device2* d3d12Device = D3D::g_Device.Get();
 	ComPtr<ID3D12Resource> uavResource = Texture.m_Resource;
-	ComPtr<ID3D12Resource> aliasResource;
 
-	struct MipGenCB
+	struct cbMipData
 	{
 		uint32_t SrcMipIndex;
 		uint32_t DestMipIndex;
@@ -114,7 +94,6 @@ void MipMapGenerator::Generate2D(Texture& Texture)
 		XMFLOAT2 TexelSize;
 	} mipGenCB{};
 
-
 	D3D::D3D12Context::GetMainHeap()->Allocate(Texture.m_SRV, 1);
 	D3D::D3D12Context::GetMainHeap()->Allocate(Texture.m_UAV, Texture.MipLevels);
 
@@ -123,22 +102,20 @@ void MipMapGenerator::Generate2D(Texture& Texture)
 
 	mipGenCB.IsSRGB = srcResourceDesc.Format == DXGI_FORMAT_R8G8B8A8_UNORM_SRGB ? 1 : 0;
 
-	//for (size_t srcMip = 0; srcMip < (srcResourceDesc.MipLevels - 1);)
-	for (size_t srcMip = 0; srcMip < (size_t)(Texture.MipLevels - 1); srcMip++) // srcMip++
+	for (size_t srcMip = 0; srcMip < (size_t)(Texture.MipLevels - 1); ++srcMip)
 	{
 		uint64_t srcWidth   = srcResourceDesc.Width >> srcMip;
 		uint32_t srcHeight  = srcResourceDesc.Height >> srcMip;
 		uint32_t destWidth  = static_cast<uint32_t>(srcWidth >> 1);
 		uint32_t destHeight = srcHeight >> 1;
 
-		mipGenCB.SrcDimension = uint32_t(((uint32_t)srcHeight & 1) << 1 | (srcWidth & 1));
+		mipGenCB.SrcDimension = static_cast<uint32_t>(((size_t)srcHeight & 1) << 1 | (srcWidth & 1));
 		DWORD mipCount = 0;
 
-		_BitScanForward(&mipCount, (destWidth == 1 ? destHeight : destWidth) |
-						(destHeight == 1 ? destWidth : destHeight));
+		_BitScanForward(&mipCount, (destWidth == 1 ? destHeight : destWidth) | (destHeight == 1 ? destWidth : destHeight));
 
 		//mipCount = std::min<DWORD>(4, mipCount - 1);
-		mipCount = std::min<DWORD>(1, mipCount - 1);
+		mipCount = std::min<long>(1, mipCount - 1);
 		mipCount = (srcMip + mipCount) >= Texture.MipLevels ? Texture.MipLevels - srcMip - 1 : mipCount;
 	
 		destWidth  = std::max(1u, destWidth);
@@ -154,18 +131,18 @@ void MipMapGenerator::Generate2D(Texture& Texture)
 		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
 		srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 		srvDesc.Texture2D.MostDetailedMip = 0;
-		srvDesc.Texture2D.MipLevels = Texture.MipLevels;// - 1
+		srvDesc.Texture2D.MipLevels = Texture.MipLevels;
 
-		d3d12Device->CreateShaderResourceView(uavResource.Get(), &srvDesc, Texture.m_SRV.GetCPU());
+		D3D::g_Device.Get()->CreateShaderResourceView(uavResource.Get(), &srvDesc, Texture.m_SRV.GetCPU());
 
 		for (size_t mip = 0; mip < mipCount; ++mip)
 		{
-			D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
+			D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc{};
 			uavDesc.Format = srcResourceDesc.Format;
 			uavDesc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
 			uavDesc.Texture2D.MipSlice = srcMip + mip + 1;
 
-			d3d12Device->CreateUnorderedAccessView(uavResource.Get(), nullptr, &uavDesc, { Texture.m_UAV.GetCPU().ptr + ((srcMip + mip) * d3d12Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)) });
+			D3D::g_Device.Get()->CreateUnorderedAccessView(uavResource.Get(), nullptr, &uavDesc, { Texture.m_UAV.GetCPU().ptr + ((srcMip + mip) * D3D::g_Device.Get()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)) });
 		}
 
 		D3D::g_CommandList->SetDescriptorHeaps(1, D3D::D3D12Context::GetMainHeap()->HeapAddressOf());
@@ -175,8 +152,6 @@ void MipMapGenerator::Generate2D(Texture& Texture)
 
 		D3D::g_CommandList->SetComputeRoot32BitConstants(0, 8, &mipGenCB, 0);
 
-		//uint32_t dispatchers{ (destWidth + 8u - 1) / 8u };
-		//D3D::g_CommandList->Dispatch(dispatchers, dispatchers, 1);
 		D3D::g_CommandList->Dispatch(((destWidth + 8u - 1) / 8u), (destHeight + 8u - 1) / 8u, 1);
 
 		CD3DX12_RESOURCE_BARRIER uavBarrier = CD3DX12_RESOURCE_BARRIER::UAV(uavResource.Get());
--- LowerDeck Engine/Engine/Graphics/MipMapGenerator.hpp
+++ LowerDeck Engine/Engine/Graphics/MipMapGenerator.hpp
@@ -6,34 +6,39 @@
 namespace D3D
 {
 	class D3D12Descriptor;
+	class D3D12RootSignature;
+	class D3D12PipelineState;
 }
-
-struct cbMipMap
-{
-	uint32_t SourceMipLevel;
-	uint32_t MipCount;
-	uint32_t SourceDims;
-	uint32_t IsSRGB;
-	DirectX::XMFLOAT2 TexelSize;
-
-};
 
 class MipMapGenerator
 {
 public:
+	~MipMapGenerator()
+	{
+		if (m_RootSignature)
+		{
+			m_RootSignature->Release();
+			m_RootSignature = nullptr;
+		}
+
+		if (m_ComputePipeline)
+		{
+			m_ComputePipeline->Release();
+			m_ComputePipeline = nullptr;
+		}
+	}
+
 	//https://github.com/EldarMuradov/EraEngine/blob/master/Source/Graphics/Backend/RenderBackend.cpp#L147
 	void CreateComputeState(std::weak_ptr<gfx::ShaderManager> wpShaderManager);
 
+
 	static ID3D12RootSignature* m_RootSignature;
 	static ID3D12PipelineState* m_ComputePipeline;
-	static ID3D12Resource*		m_Textures[4];
+
+	gfx::Shader* m_ComputeShader;
 
 	// https://github.com/EldarMuradov/EraEngine/blob/master/Source/Graphics/Backend/CommandList.cpp#L233
 	// https://github.com/EldarMuradov/EraEngine/blob/master/Resources/Shaders/MipMapGen_CS.hlsl
 	static void Generate2D(Texture& Texture);
 
-	static D3D::D3D12Descriptor srvDescriptors;
-	static D3D::D3D12Descriptor uavDescriptors;
-
 };
-
