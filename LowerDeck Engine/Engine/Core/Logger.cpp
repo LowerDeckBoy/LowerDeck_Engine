44a739248e40097402579a2e1f5ba1f30006d784
LowerDeckBoy <pa.krawiec@onet.pl>
2023-11-21 14:52:44 +0100

    Initial logging functionality

--- LowerDeck Engine/Engine/Core/Logger.cpp
+++ LowerDeck Engine/Engine/Core/Logger.cpp
@@ -1,17 +1,3 @@
 #include "Logger.hpp"
 
-void Logger::Log(const char* Message)
-{
-	//ImGuiTextBuffer buffer;
-
-   //int old_size = m_Buffer.size();
-   //va_list args;
-   //va_start(args, Message);
-   //m_Buffer.appendfv(Message, args);
-   //va_end(args);
-   //for (int new_size = m_Buffer.size(); old_size < new_size; old_size++)
-   //    if (m_Buffer[old_size] == '\n')
-   //        m_LineOffsets.push_back(old_size);
-   //m_ScrollToBottom = true;
-
-}
+std::vector<std::string> Logger::Logs = {};
--- LowerDeck Engine/Engine/Core/Logger.hpp
+++ LowerDeck Engine/Engine/Core/Logger.hpp
@@ -1,19 +1,37 @@
 #pragma once
 #include <ImGui/imgui.h>
+#include <string>
+#include <vector>
 
-
-/// <summary>
-/// Logger class meant for ImGui output usage.
-/// </summary>
 class Logger
 {
 public:
-	static void Log(const char* Message);
+	static void PrintAll()
+	{
+		for (const auto& log : Logs)
+		{
+			ImGui::Text(log.c_str());
+		}
+	}
 
-private:
-	static ImGuiTextBuffer m_Buffer;
-	static ImGuiTextFilter m_Filter;
-	static ImVector<int>   m_LineOffsets; // Index to lines offset
-	static bool            m_ScrollToBottom;
+	static void ClearLogs()
+	{
+		if (ImGui::Button("Clear logs"))
+		{
+			Logs.clear();
+		}
+	}
+
+	static std::vector<std::string> Logs;
+
 };
+
+#define LOG_INFO(Message)		Logger::Logs.push_back(std::string("[Info] " + std::string(Message)))
+#define LOG_WARN(Message)		Logger::Logs.push_back(std::string("[Warn] " + std::string(Message)))
+#define LOG_ERROR(Message)		Logger::Logs.push_back(std::string("[Error] " + std::string(Message)))
+#define LOG_CRITICAL(Message)	Logger::Logs.push_back(std::string("[Critical] " + std::string(Message)))
+
+#if defined _DEBUG
+#define LOG_DEBUG(DebugMessage) Logger::Logs.push_back(std::string("[Debug] " + std::string(DebugMessage)))
+#endif
 
